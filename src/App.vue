720d4478eb33f3b40f3a2b32dd646abbbdb86228
salvadortw <amaroam71@gmail.com>
2024-09-24 21:16:19 -0300

    Se agrega el proyecto

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 
 import { defineComponent } from 'vue';
 
-import TabsMenu from '@components/TabsMenu.vue'
+import TabsMenu from './components/TabsMenu.vue';
 
 export default defineComponent({
     name: 'App',
-    components: { IonApp, IonRouterOutlet },
+    components: { IonApp, IonRouterOutlet, TabsMenu, },
   });
 </script>
